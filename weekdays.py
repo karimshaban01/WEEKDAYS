e66f5a57d0d1a6d37ad9097971826d68d55b3d08
karimshaban01 <94116967+karimshaban01@users.noreply.github.com>
2022-05-07 17:40:24 +0300

    Add files via upload

---
+++ weekdays.py
@@ -0,0 +1,24 @@
+day = str(input('enter a day: '))
+if day == 'saturday':
+    print('happy sabbath day')
+
+elif day == 'sunday':
+    print('have nice worship')
+
+elif day == "monday":
+    print('wake up early prepare yourself for daytime tasks')
+
+elif day == "tuesday":
+    print('wake up early prepare yourself for daytime tasks')
+
+elif day == "wednsday":
+    print('wake up early prepare yourself for daytime tasks')
+
+elif day == "thursday":
+    print('wake up early prepare yourself for daytime tasks')
+
+elif day == "friday":
+    print('IJUMAA MUBAARAK')
+
+else:
+    print('enter correct day')
